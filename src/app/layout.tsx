6f7b486fc0cbf914a9a11ff892309712b1991a64
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-30 02:49:50 +0000

    mude o icon do webapp para algo administrativo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,11 @@ const nunito = Nunito({
 });
 
 export const metadata: Metadata = {
-  title: 'bemmu. - Gestão Inteligente para Cantinas Escolares',
+  title: 'Bemmu. - Gestão Inteligente para Cantinas Escolares',
   description: 'Otimize pedidos, pagamentos e estoque da sua cantina escolar. Menos filas, mais controle e segurança para pais, alunos e escolas.',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📊</text></svg>',
+  },
 };
 
 export default function RootLayout({
